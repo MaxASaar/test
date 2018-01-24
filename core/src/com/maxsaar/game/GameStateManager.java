69921c96c45f134c840cf51d3253c0530704f73c
Maximilian Saar <masaar@uwaterloo.ca>
2018-01-24 16:16:29 -0500

    android runs from game directory not test, took useless things out of manifest

--- core/src/com/maxsaar/game/GameStateManager.java
+++ core/src/com/maxsaar/game/GameStateManager.java
@@ -3,11 +3,6 @@ package com.maxsaar.game;
 /**
  * Created by Max on 23/01/2018.
  */
-
-enum gameStates{
-    MAIN_MENU,
-    GAME
-}
 public class GameStateManager {
 
     public GameStateManager(){
---
+++ core/src/com/maxsaar/game/gameStates.java
@@ -0,0 +1,11 @@
+package com.maxsaar.game;
+
+/**
+ * Created by Max on 24/01/2018.
+ */
+
+
+enum gameStates{
+    MAIN_MENU,
+    GAME
+}
